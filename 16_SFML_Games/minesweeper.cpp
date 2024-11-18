a7cbd277c88f9a93cbe2f432d12ac2cd65f65581
(Student  C00300324) Jack Kane <C00300324@setu.ie>
2024-11-18 12:19:02 +0000

    removed magic nums

--- 16_SFML_Games/minesweeper.cpp
+++ 16_SFML_Games/minesweeper.cpp
@@ -4,6 +4,7 @@ using namespace sf;
 
 int minesweeper()
 {
+    int mine = 9;
     srand(time(0));
 
     RenderWindow app(VideoMode(400, 400), "Minesweeper!");
@@ -11,7 +12,7 @@ int minesweeper()
     int tileSize=32;
     int grid[12][12];
     int shownGrid[12][12]; //for showing
-
+    int mineChance = 5;//1 in 5
     Texture texture;
     texture.loadFromFile("images/minesweeper/tiles.jpg");
     Sprite sprite(texture);
@@ -20,7 +21,7 @@ int minesweeper()
      for (int column =1; column <=10;column++)
       {
         shownGrid[row][column]=10;
-        if (rand()%5==0)  grid[row][column]=9;
+        if (rand()%mineChance==0)  grid[row][column]=9;
         else grid[row][column]=0;
       }
 
@@ -28,15 +29,15 @@ int minesweeper()
      for (int column=1;column<=10;column++)//repeats for each tile
       {
         int mineCounter=0;
-        if (grid[row][column]==9) continue; //if tile is a mine continue
-        if (grid[row+1][column]==9) mineCounter++;//checks all tiles for nearby mines, 9 = mine if found increase mine counter for number displayed
-        if (grid[row][column-1]==9) mineCounter++;
-        if (grid[row -1][column]==9) mineCounter++;
-        if (grid[row][column -1]==9) mineCounter++;
-        if (grid[row +1][column +1]==9) mineCounter++;
-        if (grid[row -1][column -1]==9) mineCounter++;
-        if (grid[row -1][column +1]==9) mineCounter++;
-        if (grid[row +1][column -1]==9) mineCounter++;
+        if (grid[row][column]==mine) continue; //if tile is a mine continue
+        if (grid[row+1][column]== mine) mineCounter++;//checks all tiles for nearby mines, 9 = mine if found increase mine counter for number displayed
+        if (grid[row][column-1]== mine) mineCounter++;
+        if (grid[row -1][column]== mine) mineCounter++;
+        if (grid[row][column -1]== mine) mineCounter++;
+        if (grid[row +1][column +1]== mine) mineCounter++;
+        if (grid[row -1][column -1]== mine) mineCounter++;
+        if (grid[row -1][column +1]== mine) mineCounter++;
+        if (grid[row +1][column -1]== mine) mineCounter++;
         grid[row][column] = mineCounter;
       }
 
@@ -62,7 +63,7 @@ int minesweeper()
         for (int row=1; row <=10; row++)
          for (int column =1; column <=10;column++)
           {
-           if (shownGrid[x][y]==9) 
+           if (shownGrid[x][y]== mine)
                shownGrid[row][column]=grid[row][column];//if mine is found show mine
            sprite.setTextureRect(IntRect(shownGrid[row][column]*tileSize,0,tileSize,tileSize));
            sprite.setPosition(row * tileSize, column * tileSize);
